999c99f9f308209acb83cbdf177eb685110ccd85
quynhuw <qinhuuuuu@gmail.com>
2024-10-16 15:48:44 +0700

    update color menu mobile

--- app/components/DropdownMenu.tsx
+++ app/components/DropdownMenu.tsx
@@ -25,7 +25,7 @@ const DropdownMenu: React.FC<DropdownMenuProps> = ({ items }) => {
 
       {isOpen && (
         <div
-          className={`animate-jump-in animate-duration-300 animate-ease-linear absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-primary shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none border border-gray-900`}
+          className={`animate-jump-in animate-duration-300 animate-ease-linear absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-[#7BCEF5] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none border border-gray-200`}
           role="menu"
           aria-orientation="vertical"
           aria-labelledby="menu-button"
